d75ee5ba97a9c176b6c124c31a8ee5be7b8e251b
Maxine Athos <heyymax@icloud.com>
2025-12-06 23:41:49 -0300

    first commit

---
+++ src/servidor.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import url from 'url';
+import path from 'path';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublico = path.join(caminhoAtual, "../..", "public");
+app.use(express.static(diretorioPublico));
+
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+const io = new Server(servidorHttp);
+
+io.on('connection', () => {
+    console.log('Um cliente se conectou!')
+})
